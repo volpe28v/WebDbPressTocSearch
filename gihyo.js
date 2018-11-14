63b3616cec9337b2f9c62451beffbda6e943692b
volpe28v <naoki.koda@gmail.com>
2018-11-15 00:21:45 +0900

    Add gihyo module

---
+++ gihyo.js
@@ -0,0 +1,43 @@
+var client = require('cheerio-httpcli');
+
+var backnumber_url = 'https://gihyo.jp/magazine/wdpress/backnumber'
+
+module.exports.search = function(keyword){
+  var keyword_reg = new RegExp('(' + keyword + ')', "gi");
+
+  return new Promise(function(resolve, reject){
+    client.fetch(backnumber_url, {})
+      .then(function(result){
+        var urls = [];
+        var promises = [];
+        result.$('.magazineList01 .data a').each(function(){
+          var url = 'https://gihyo.jp' + result.$(this).attr('href');
+          urls.push(url);
+          promises.push(client.fetch(url, {}));
+        });
+
+        return Promise.all(promises);
+      })
+      .then(function(results){
+        var found_tocs = results.map(function(result){
+          var title = result.$('title').text();
+          var toc = result.$('#toc').text();
+
+          var found_lines = [];
+          toc.split("\n").forEach(function(line){
+            if (line.match(keyword_reg)){
+              found_lines.push(line.trim());
+            }
+          });
+
+          return { title: title, found_lines: found_lines };
+        }).filter(function(toc){
+          return toc.found_lines.length > 0;
+        });
+
+        resolve(found_tocs);
+      })
+      .catch(function(err){
+      });
+  });
+}
--- index.js
+++ index.js
@@ -1,53 +1,26 @@
-var client = require('cheerio-httpcli');
+var gihyo = require('./gihyo');
 
 if (process.argv.length < 3){
   console.log("node index.js keyword");
   return;
 }
 
-var yellow  = '\u001b[33m';
-var reset   = '\u001b[0m';
-
 var keyword = process.argv[2];
-var keyword_reg = new RegExp('(' + keyword + ')', "gi");
 
-var backnumber_url = 'https://gihyo.jp/magazine/wdpress/backnumber'
+gihyo.search(keyword)
+.then(function(found_tocs){
+  out_console(keyword, found_tocs);
+});
 
-client.fetch(backnumber_url, {})
-.then(function(result){
-  var urls = [];
-  var promises = [];
-  result.$('.magazineList01 .data a').each(function(){
-    var url = 'https://gihyo.jp' + result.$(this).attr('href');
-    urls.push(url);
-    promises.push(client.fetch(url, {}));
-  });
+function out_console(keyword, found_tocs){
+  var keyword_reg = new RegExp('(' + keyword + ')', "gi");
+  var yellow  = '\u001b[33m';
+  var reset   = '\u001b[0m';
 
-  return Promise.all(promises);
-})
-.then(function(results){
-  var found_tocs = results.map(function(result){
-    var title = result.$('title').text();
-    var toc = result.$('#toc').text();
-
-    var found_lines = [];
-    toc.split("\n").forEach(function(line){
-      if (line.match(keyword_reg)){
-        found_lines.push(line.trim());
-      }
-    });
-
-    return { title: title, found_lines: found_lines };
-  }).filter(function(toc){
-    return toc.found_lines.length > 0;
-  });
-
-
-  // 結果表示
   console.log("検索キーワード: " + keyword);
   console.log("ヒットした書籍数: " + found_tocs.length + " 冊");
   found_tocs.forEach(function(toc){
     console.log("\n・" + toc.title);
     console.log("\t" + toc.found_lines.join("\n\t").replace(keyword_reg, yellow + '$1' + reset));
   });
-});
+}
