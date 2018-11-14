7fd41cf3128e78439d39a18df20795e1f2946e62
volpe28v <naoki.koda@gmail.com>
2018-11-14 16:41:15 +0900

    Add display found books count

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ var yellow  = '\u001b[33m';
 var reset   = '\u001b[0m';
 
 var keyword = process.argv[2];
-var keywordReg = new RegExp('(' + keyword + ')', "gi");
-console.log("keyword: " + keyword);
+var keyword_reg = new RegExp('(' + keyword + ')', "gi");
 
 var backnumber_url = 'https://gihyo.jp/magazine/wdpress/backnumber'
 
@@ -27,23 +26,28 @@ client.fetch(backnumber_url, {})
   return Promise.all(promises);
 })
 .then(function(results){
-  var tocs = results.map(function(result){
+  var found_tocs = results.map(function(result){
     var title = result.$('title').text();
     var toc = result.$('#toc').text();
-    return { title: title, body: toc };
-  });
 
-  tocs.forEach(function(toc){
-    var foundLines = [];
-    toc.body.split("\n").forEach(function(line){
-      if (line.match(keywordReg)){
-        foundLines.push(line.trim());
+    var found_lines = [];
+    toc.split("\n").forEach(function(line){
+      if (line.match(keyword_reg)){
+        found_lines.push(line.trim());
       }
     });
 
-    if (foundLines.length > 0){
-      console.log("\n・" + toc.title);
-      console.log("\t" + foundLines.join("\n\t").replace(keywordReg, yellow + '$1' + reset));
-    }
+    return { title: title, found_lines: found_lines };
+  }).filter(function(toc){
+    return toc.found_lines.length > 0;
+  });
+
+
+  // 結果表示
+  console.log("検索キーワード: " + keyword);
+  console.log("ヒットした書籍数: " + found_tocs.length + " 冊");
+  found_tocs.forEach(function(toc){
+    console.log("\n・" + toc.title);
+    console.log("\t" + toc.found_lines.join("\n\t").replace(keyword_reg, yellow + '$1' + reset));
   });
 });
